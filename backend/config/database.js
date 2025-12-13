d83ce291d317ea99c9af399696914bf306bff899
Deepak Dhakad <145159816+DeepakDkd@users.noreply.github.com>
2025-12-13 17:14:19 +0530

    live

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,28 +1,28 @@
-// const path = require("path");
-
-// module.exports = ({ env }) => {
-//   return {
-//     connection: {
-//       client: "postgres",
-//       connection: {
-//         host: env("PGHOST", "127.0.0.1"),
-//         port: env.int("PGPORT", 24569),
-//         database: env("PGDATABASE", "strapi"),
-//         user: env("PGUSER", "strapi"),
-//         password: env("PGPASSWORD", "password"),
-//         // ssl: true,
-//       },
-//     },
-//   };
-// };
 const path = require("path");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: "sqlite",
+module.exports = ({ env }) => {
+  return {
     connection: {
-      filename: path.join(__dirname, "..", ".tmp", "data.db"),
+      client: "postgres",
+      connection: {
+        host: env("PGHOST", "127.0.0.1"),
+        port: env.int("PGPORT", 24569),
+        database: env("PGDATABASE", "strapi"),
+        user: env("PGUSER", "strapi"),
+        password: env("PGPASSWORD", "password"),
+        ssl: true,
+      },
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};
+// const path = require("path");
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "sqlite",
+//     connection: {
+//       filename: path.join(__dirname, "..", ".tmp", "data.db"),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });

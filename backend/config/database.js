5d60478e8c6dd06b371f9aa9f7dd1904e9bf89cb
Deepak Dhakad <145159816+DeepakDkd@users.noreply.github.com>
2025-12-13 18:16:24 +0530

    config updated

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,28 +1,31 @@
 const path = require("path");
 
 module.exports = ({ env }) => {
+  // PRODUCTION (Railway)
+  if (env("NODE_ENV") === "production") {
+    return {
+      connection: {
+        client: "postgres",
+        connection: {
+          host: env("PGHOST"),
+          port: env.int("PGPORT"),
+          database: env("PGDATABASE"),
+          user: env("PGUSER"),
+          password: env("PGPASSWORD"),
+          ssl: { rejectUnauthorized: false },
+        },
+      },
+    };
+  }
+
+  // LOCAL DEVELOPMENT
   return {
     connection: {
-      client: "postgres",
+      client: "sqlite",
       connection: {
-        host: env("PGHOST", "127.0.0.1"),
-        port: env.int("PGPORT", 24569),
-        database: env("PGDATABASE", "strapi"),
-        user: env("PGUSER", "strapi"),
-        password: env("PGPASSWORD", "password"),
-        ssl: true,
+        filename: path.join(__dirname, "..", ".tmp", "data.db"),
       },
+      useNullAsDefault: true,
     },
   };
 };
-// const path = require("path");
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: "sqlite",
-//     connection: {
-//       filename: path.join(__dirname, "..", ".tmp", "data.db"),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });

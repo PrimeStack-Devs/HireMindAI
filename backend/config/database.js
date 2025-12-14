e179e713379b0d2107768e1b5fd9629e4495bac1
Deepak Dhakad <145159816+DeepakDkd@users.noreply.github.com>
2025-12-14 15:38:20 +0530

    fixed db error

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,31 +1,48 @@
+// const path = require("path");
+
+// module.exports = ({ env }) => {
+//   // PRODUCTION (Railway)
+//   if (env("NODE_ENV") === "production") {
+//     return {
+//       connection: {
+//         client: "postgres",
+//         connection: {
+//           host: env("PGHOST"),
+//           port: env.int("PGPORT"),
+//           database: env("PGDATABASE"),
+//           user: env("PGUSER"),
+//           password: env("PGPASSWORD"),
+//           ssl: { rejectUnauthorized: false },
+//         },
+//       },
+//     };
+//   }
+
+//   // LOCAL DEVELOPMENT
+//   return {
+//     connection: {
+//       client: "sqlite",
+//       connection: {
+//         filename: path.join(__dirname, "..", ".tmp", "data.db"),
+//       },
+//       useNullAsDefault: true,
+//     },
+//   };
+// };
 const path = require("path");
 
 module.exports = ({ env }) => {
-  // PRODUCTION (Railway)
-  if (env("NODE_ENV") === "production") {
-    return {
-      connection: {
-        client: "postgres",
-        connection: {
-          host: env("PGHOST"),
-          port: env.int("PGPORT"),
-          database: env("PGDATABASE"),
-          user: env("PGUSER"),
-          password: env("PGPASSWORD"),
-          ssl: { rejectUnauthorized: false },
-        },
-      },
-    };
-  }
-
-  // LOCAL DEVELOPMENT
   return {
     connection: {
-      client: "sqlite",
+      client: "postgres",
       connection: {
-        filename: path.join(__dirname, "..", ".tmp", "data.db"),
+        host: env("PGHOST", "127.0.0.1"),
+        port: env.int("PGPORT", 24569),
+        database: env("PGDATABASE", "strapi"),
+        user: env("PGUSER", "strapi"),
+        password: env("PGPASSWORD", "password"),
+        // ssl: true,
       },
-      useNullAsDefault: true,
     },
   };
 };
